16d6c413b090ba5f763ea883bb8f8bc61d2ef8ba
AK <akrivit@gmail.com>
2023-10-15 00:00:35 -0400

    get capture mechanism working

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,6 @@
-import browser from 'webextension-polyfill';
+import browser, { type WebRequest } from 'webextension-polyfill';
 import { detect } from 'detect-browser';
-import {
-  type BrowserMessage,
-  type BrowserMessageType,
-  type ColorScheme
-} from './models';
+import type { BrowserMessage, BrowserMessageType, ColorScheme } from './models';
 import settingsConnector from './settings-connector';
 
 console.log('background script running...');
@@ -22,4 +18,46 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
 async function updateIcon(colorScheme: ColorScheme) {
   console.log('updating icon', colorScheme);
   // do work here
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  handleCaughtRequest,
+  { urls: ['*://*/*track*', '*://*/*engage*'] },
+  ['requestBody']
+);
+
+function handleCaughtRequest(details: WebRequest.OnBeforeRequestDetailsType) {
+  // Access the URL and request body
+  const url = details.url;
+  storeRequest(details);
+  console.log('Request URL:', url);
+
+  if (details.requestBody && details.requestBody.raw) {
+    console.log('Request Body:', details.requestBody);
+  }
+}
+
+// To capture the server response:
+browser.webRequest.onCompleted.addListener(
+  details => {
+    console.log('Response for:', details.url);
+    console.log('Status:', details.statusCode);
+    // Note: This doesn't capture the response body. Capturing response body is trickier and requires additional handling.
+  },
+  { urls: ['*://*/*track*'] }
+);
+
+function storeRequest(details: WebRequest.OnBeforeRequestDetailsType) {
+  const { url, requestBody, initiator, incognito, timeStamp, type } = details;
+  let records = [];
+  if (requestBody?.formData) {
+    try {
+      records = JSON.parse(requestBody.formData.data);
+    } catch (e) {
+      console.log('error parsing json', e);
+    }
+  }
+
+  console.log(records);
+
 }
--- src/popup/popup.svelte
+++ src/popup/popup.svelte
@@ -2,13 +2,17 @@
   console.log('hello from the popup!');
 </script>
 
-<div>
-  <h1>My New Web Extension</h1>
+<div id="main">
+  <h1>Mixpanel Debug</h1>
 </div>
 
 <style>
-  div {
+  div#main {
     display: flex;
     justify-content: center;
+	width: 100%;
+	height: 100%;
   }
+
+
 </style>
