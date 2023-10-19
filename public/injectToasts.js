f90a4570011b8c5c9af62245bd1903286b65eb4b
AK <akrivit@gmail.com>
2023-10-18 23:42:50 -0400

    toasts are good!

--- public/injectToasts.js
+++ public/injectToasts.js
@@ -1,17 +1,96 @@
-// a hack because of the way we've implemented the toast component as a web component in a shadow dom (not directly accessible to content scripts)
-document.addEventListener("showToastEvent", function (e) {	
+document.addEventListener("showToastEvent", function (e) {
 	try {
-		const message = e.detail.message;
-		//? https://github.com/mixpanel/analytics/tree/master/iron/common/components/toast
-		document.querySelector("#MIXPANEL_DEBUG_ROOT").shadowRoot.querySelector("#toast-area").addToast({
+		const ROOT = document.querySelector("#MIXPANEL_DEBUG_ROOT").shadowRoot;
+		const toastArea = ROOT.querySelector("#toast-area");
+		let message = e.detail.message;
+		const record = e.detail.record;
+		const type = e.detail.type;
+		//? https://github.com/mixpanel/analytics/blob/master/iron/common/components/toast-area/index.ts#L21
+		const toastProps = {
 			message,
-			cta: 'go to link',
+			cta: "→",
 			hide: false,
-			type: 'info',
-			timeout: 5000000000
-		});
-	}
-	catch (err) {
+			type: "success",
+			
+			timeout: 3000,
+			closeToastCallback: () => {
+				// make sure toasts don't stay mounted to the DOM
+				setTimeout(() => {
+					toastArea.clearAllToasts();
+				}, 5000);
+			},
+			ctaCallback: () => {
+				// this is not displaying correctly
+				const infoModal = document.createElement("mp-modal");
+				infoModal.setAttribute(`closeable`, `true`);
+				infoModal.setAttribute(`open`, `true`);
+				infoModal.setAttribute(`modal-type`, `popup`);
+				infoModal.setAttribute(`grow-to-content`, `true`);
+				infoModal.innerHTML = `
+				<div class="mp-modal-content">
+				${type === "event" ? MIXPANEL_EVENT_ICON : MIXPANEL_PROFILE_ICON}
+					<div class="mp-modal-title">${type}</div>
+					<div class="mp-modal-subtitle">
+						<pre>${prettyJson(record)}</pre>
+					</div>
+				</div>`;
+				ROOT.appendChild(infoModal);
+			},
+		};
+
+		//this works great!
+		toastArea.addToast(toastProps);
+	} catch (err) {
 		console.log(`failed to show toast`, err);
 	}
 });
+
+function prettyJson(data, ignoreKeys = [], indentLevel = 0) {
+	if (!data || typeof data !== "object") return data;
+
+	let prettyOpts = ``;
+	let indent = "\t".repeat(indentLevel);
+
+	for (const key in data) {
+		if (data.hasOwnProperty(key) && !ignoreKeys.includes(key)) {
+			let val = data[key];
+
+			// Skip if the value is undefined
+			if (val === undefined) continue;
+
+			if (typeof val === "function") {
+				val = val.toString();
+			} else if (typeof val === "object") {
+				if (val === null) {
+					val = "null";
+				} else if (Array.isArray(val) || val instanceof Set) {
+					// @ts-ignore
+					if (!val.size && val instanceof Set) continue; // Check if set is empty
+					// @ts-ignore
+					if (!val.length && Array.isArray(val)) continue; // Check if array is empty
+					val = Array.from(val)
+						.map((item) => prettyJson(item, ignoreKeys, indentLevel + 1))
+						.join(",\n");
+				} else {
+					// Check if object is empty and represent it as "{}"
+					try {
+						if (Object.keys(val).length === 0) {
+							val = "{}";
+						} else {
+							val = "\n\t" + prettyJson(val, ignoreKeys, indentLevel + 1) + "\n" + indent;
+						}
+					} catch (e) { }
+				}
+			}
+
+			prettyOpts += `${indent}${key}: ${val}\n`;
+		}
+	}
+
+	return prettyOpts.trim();
+}
+
+const MIXPANEL_EVENT_ICON = `<svg-icon icon="event"><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><path d="M9.014 9.64l3.068 8.766a.2.2 0 00.381-.01l1.376-4.803 4.54-1.297a.2.2 0 00.008-.382L9.644 9a.5.5 0 00-.63.64z" stroke="currentColor" stroke-width="1.5"></path><path d="M9 3v2M3 9h2M4.786 13.164L6.2 11.75M13.573 4.813L12.16 6.228M4.814 4.813l1.414 1.415" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path></svg></svg-icon>`
+
+const MIXPANEL_PROFILE_ICON = `<svg-icon icon="profile"><svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><path d="M13.83 15.5a1 1 0 0 0 1-1.28A4 4 0 0 0 11 11.5h0a4 4 0 0 0-3.79 2.72 1 1 0 0 0 1 1.28z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></path><circle cx="11" cy="8.5" r="2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></circle><rect x="3" y="3" width="16" height="16" rx="2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></rect></svg>
+</svg-icon>`
--- src/toast.ts
+++ src/toast.ts
@@ -1,119 +1,113 @@
-import browser from 'webextension-polyfill';
-import { type ParsedRequest } from './models';
-import Toastify from 'toastify-js';
-import { prettyJson } from './utils';
+import browser from "webextension-polyfill";
+import { type ParsedRequest } from "./models";
+import Toastify from "toastify-js";
+import { prettyJson } from "./utils";
 
-console.log('CONTENT SCRIPT RAN');
+console.log("CONTENT SCRIPT RAN");
 let shadowCache: ShadowRoot | null = null;
 
-const profileOperations = [
-  '$set',
-  '$set_once',
-  '$unset',
-  '$append',
-  '$remove',
-  '$union',
-  '$delete'
-];
+const profileOperations = ["$set", "$set_once", "$unset", "$append", "$remove", "$union", "$delete"];
+
+createToastWithShadowDOM();
 
 browser.runtime.onMessage.addListener((data: ParsedRequest) => {
-  const { records = [], endpoint } = data;
-
-  //refactor
-  for (const record of records) {
-    let message;
-    let type;
-    if (endpoint === '/track') {
-      const name = record.event;
-      type = 'event';
-      message = `event: ${name}`;
-    } else if (endpoint === '/engage') {
-      const distinct_id = record.$distinct_id;
-      const operation =
-        Object.keys(record)
-          .filter(key => profileOperations.includes(key))
-          .slice()
-          .pop() || '$unknown';
-      type = 'profile';
-      message = `profile ${operation}: ${distinct_id}`;
-    } else {
-      message = `something isn't right`;
-      console.error(`Record`, record, `Endpoint`, endpoint);
-    }
-
-    try {
-      createToastWithShadowDOM(message, record);
-    } catch (e) {
-      console.log('error creating toast', e);
-      debugger;
-    }
-  }
+	const { records = [], endpoint } = data;
+	handleRecords(records, endpoint);
 });
 
+function handleRecords(records: Array<Object>, endpoint: string) {
+	//refactor
+	for (const record of records) {
+		let message;
+		let type;
+		if (endpoint === "/track") {
+			const name = record?.event || "none";
+			type = "event";
+			message = `${name}`;
+		} else if (endpoint === "/engage") {
+			const distinct_id = record?.$distinct_id || "none";
+			const operation =
+				Object.keys(record)
+					.filter((key) => profileOperations.includes(key))
+					.slice()
+					.pop() || "$unknown";
+			type = "profile";
+			message = `profile ${operation}: ${distinct_id}`;
+		} else {
+			message = `something isn't right`;
+			console.error(`Record`, record, `Endpoint`, endpoint);
+		}
 
-function createToastWithShadowDOM(message: string, record: Object) {
-  let shadow;
-  // Create a new div to attach the shadow root
-  if (!shadowCache) {
-    const toastContainer = document.createElement('div');
-	toastContainer.id = 'MIXPANEL_DEBUG_ROOT';
-    document.body.appendChild(toastContainer);
-
-    shadow = toastContainer.attachShadow({ mode: 'open' });
-    injectAssets(shadow, () => {
-      triggerShowToastInPage(message);
-    });
-
-    shadowCache = shadow;
-  } else {
-    shadow = shadowCache;
-  }
-  triggerShowToastInPage(message);
+		try {
+			createToastWithShadowDOM(message, record, type);
+		} catch (e) {
+			console.log("error creating toast", e);
+			debugger;
+		}
+	}
 }
 
+function createToastWithShadowDOM(message?: string, record?: Object, type?: string) {
+	let shadow;
+	// Create a new div to attach the shadow root
+	if (!shadowCache) {
+		const toastContainer = document.createElement("div");
+		toastContainer.id = "MIXPANEL_DEBUG_ROOT";
+		document.body.appendChild(toastContainer);
 
-function triggerShowToastInPage(message = "foo") {
-  const event = new CustomEvent('showToastEvent', {
-    detail: { message }
-  });
-  document.dispatchEvent(event);
+		shadow = toastContainer.attachShadow({ mode: "open" });
+		injectAssets(shadow, () => {
+			if (message) triggerShowToastInPage(message, record, type);
+		});
+
+		shadowCache = shadow;
+	} else {
+		shadow = shadowCache;
+	}
+	if (message) triggerShowToastInPage(message, record, type);
 }
 
+function triggerShowToastInPage(message = "foo", record?: Object, type?: string) {
+	const event = new CustomEvent("showToastEvent", {
+		detail: { message, record, type },
+	});
+	document.dispatchEvent(event);
+}
 
 // essentially bundling our entire design system...
 function injectAssets(shadowDom: ShadowRoot, callback: Function) {
-	let scriptsToLoad = 4; 
+	let scriptsToLoad = 4;
 	const assets = [];
-  
+
 	// CSS assets
-	const cssFiles = ['public/mixpanel.css', 'public/bundle.min.css'];
-	cssFiles.forEach(file => {
-	  const link = document.createElement('link');
-	  link.rel = 'stylesheet';
-	  link.href = browser.runtime.getURL(file);
-	  shadowDom.appendChild(link);
+	const cssFiles = ["public/mixpanel.css", "public/bundle.min.css"];
+	cssFiles.forEach((file) => {
+		const link = document.createElement("link");
+		link.rel = "stylesheet";
+		link.href = browser.runtime.getURL(file);
+		shadowDom.appendChild(link);
 	});
-  
+
 	// JS assets
 	const jsFiles = [
-	  'public/iron.min.js',
-	  'public/runtime.min.js',
-	  'public/node_modules.min.js',
-	  'public/injectToasts.js'
+		"public/iron.min.js",
+		"public/runtime.min.js",
+		"public/node_modules.min.js",
+		"public/injectToasts.js",
 	];
-	jsFiles.forEach(file => {
-	  const script = document.createElement('script');
-	  script.onload = () => {
-		scriptsToLoad--;
-		if (scriptsToLoad === 0) {
-		  // Create the <mp-toast-area> after all scripts have loaded
-		  const toastArea = document.createElement('mp-toast-area');
-		  toastArea.id = 'toast-area';
-		  shadowDom.appendChild(toastArea);
-		  callback();
-		}
-	  };
-	  script.src = browser.runtime.getURL(file);
-	  shadowDom.appendChild(script);
+	jsFiles.forEach((file) => {
+		const script = document.createElement("script");
+		script.onload = () => {
+			scriptsToLoad--;
+			if (scriptsToLoad === 0) {
+				// Create the <mp-toast-area> after all scripts have loaded
+				const toastArea = document.createElement("mp-toast-area");
+				toastArea.id = "toast-area";
+				shadowDom.appendChild(toastArea);
+				callback();
+			}
+		};
+		script.src = browser.runtime.getURL(file);
+		shadowDom.appendChild(script);
 	});
-  }
+}
